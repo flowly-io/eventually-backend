5fec9655401979209bf2a3e0e1bd462b937aed55
Ben Yap <bwyap@outlook.com>
2019-04-06 11:11:59 +1100

    feat: modularise setup

---
+++ src/graphql/typeDefs/index.js
@@ -0,0 +1,21 @@
+import { gql } from "apollo-server";
+
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+const typeDefs = gql`
+  # Comments in GraphQL are defined with the hash (#) symbol.
+
+  # This "Book" type can be used in other type declarations.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is the root of all GraphQL queries.
+  # (A "Mutation" type will be covered later on.)
+  type Query {
+    books: [Book]
+  }
+`;
+
+export { typeDefs };
